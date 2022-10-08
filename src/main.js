42af4a87069d9534ede99d5a279321e2c0a3c33d
ALT <andersonlottermann@gmail.com>
2022-10-08 20:49:40 -0300

    atualized

--- src/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-var port = 3000;
-app.get('/', function (req, res) { return res.send('Hello World!'); });
-app.listen(port, function () { return console.log("Server started listening to port ".concat(port)); });
