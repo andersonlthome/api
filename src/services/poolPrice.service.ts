55cf15efd9c6cb93d105a30b11ba68533e3d1872
ALT <andersonlottermann@gmail.com>
2022-10-09 03:28:48 -0300

    get funcionando

--- src/services/poolPrice.service.ts
+++ src/services/poolPrice.service.ts
@@ -43,25 +43,32 @@ async function createPool() {
     getPoolImmutables(),
     getPoolState(),
   ]);
+  console.log(immutables, state);
   const ctTokenA = await getContract(immutables.token0, ERC20ABI);
   const ctTokenB = await getContract(immutables.token1, ERC20ABI);
+  
+  console.log('ctt', ctTokenA.address, ctTokenB.address);
 
-  const TokenA = new Token(
-    3, //chainId,
-    immutables.token0,
-    ctTokenA.decimals(),
-    ctTokenA.symbol(),
-    ctTokenA.name()
-  );
-  const TokenB = new Token(
-    3, //chainId,
-    immutables.token1,
-    ctTokenB.decimals(),
-    ctTokenB.symbol(),
-    ctTokenB.name()
-  );
+  // Token dando erro
+  const TokenA = new Token(3, immutables.token0, 6, 'USDC', 'USD Coin')
+  const TokenB = new Token(3, immutables.token1, 18, 'WETH', 'Wrapped Ether')
 
-  console.log(TokenA, TokenB);
+  // const TokenA = new Token(
+  //   1, //chainId,
+  //   immutables.token0,
+  //   ctTokenA.decimals(),
+  //   ctTokenA.symbol(),
+  //   ctTokenA.name()
+  // );
+  // const TokenB = new Token(
+  //   1, //chainId,
+  //   immutables.token1,
+  //   ctTokenB.decimals(),
+  //   ctTokenB.symbol(),
+  //   ctTokenB.name()
+  // );
+
+  console.log('tokens', TokenA, TokenB);
 
   const pool = new Pool(
     TokenA,
@@ -71,11 +78,12 @@ async function createPool() {
     state.liquidity.toString(),
     state.tick
   );
+  console.log('pool', pool);
 
   return pool;
 }
 
-async function getContract(abi: any, signer?: any) {
+async function getContract(address:any ,abi: any, signer?: any) {
   const poolAddress = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8";
 
   // const infuraKey = process.env.INFURA_KEY;
@@ -100,14 +108,17 @@ async function getContract(abi: any, signer?: any) {
     tokens.eth.provider_url
   );
 
-  return new ethers.Contract(poolAddress, abi, signer ? signer : provider);
+  const contractAddress = address ?? poolAddress;
+  return new ethers.Contract(contractAddress, abi, signer ? signer : provider);
   // return new ethers.Contract(contractAddress, abi, signer ? signer : provider);
 }
 
 // async function getPoolImmutables(poolAddress: string) {
 async function getPoolImmutables() {
+  const poolAddress = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8";
+
   // const poolContract = await getContract(poolAddress, IUniswapV3PoolABI);
-  const poolContract = await getContract(IUniswapV3PoolABI);
+  const poolContract = await getContract(poolAddress, IUniswapV3PoolABI);
 
   const [factory, token0, token1, fee, tickSpacing, maxLiquidityPerTick] =
     await Promise.all([
@@ -131,7 +142,8 @@ async function getPoolImmutables() {
 }
 
 async function getPoolState() {
-  const poolContract = await getContract(IUniswapV3PoolABI);
+  const poolAddress = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8";
+  const poolContract = await getContract(poolAddress,IUniswapV3PoolABI);
 
   const [liquidity, slot] = await Promise.all([
     poolContract.liquidity(),
